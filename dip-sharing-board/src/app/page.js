b081159a233660a22490f98f4dfd5254941572a6
tatchapong <specter15564@gmail.com>
2024-07-30 01:54:41 +0700

    make template for poster and adjust position it

--- dip-sharing-board/src/app/page.js
+++ dip-sharing-board/src/app/page.js
@@ -5,90 +5,36 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <div className={styles.descriptionp}>DIP Sharing Board</div>
       </div>
-
+      <div className={styles.description1}>Camp</div>
+      <div className={styles.description2}>Competition</div>
+      <div className={styles.description3}>Others</div>
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="campcard.svg"
           alt="Next.js Logo"
-          width={180}
+          width={600}
           height={37}
           priority
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <Image
+          className={styles.logo1}
+          src="competitioncard.svg"
+          alt="Next.js Logo"
+          width={600}
+          height={37}
+          priority
+        />
+        <Image
+          className={styles.logo2}
+          src="otherscard.svg"
+          alt="Next.js Logo"
+          width={600}
+          height={37}
+          priority
+        />
       </div>
     </main>
   );
